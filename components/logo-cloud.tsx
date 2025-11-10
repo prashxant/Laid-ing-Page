9e78791af60891c528727e7f30a0d3777f019a6c
prashxant <notxdead0@gmail.com>
2025-11-10 11:23:55 +0530

    wow

---
+++ components/logo-cloud.tsx
@@ -0,0 +1,22 @@
+
+
+export const LogoCloud = () => {
+  const logos = [
+    {
+      title: "Open AI",
+      src: "https://assets.aceternity.com/logos/openai.png",
+    },
+    {
+      title: "Hello Patient",
+      src: "https://assets.aceternity.com/logos/openai.png",
+    },
+    {
+      title: "Granola",
+      src: "https://assets.aceternity.com/logos/openai.png",
+    },
+  ];
+  return (
+    <div>Logo-cloud</div>
+  )
+}
+
